461cc4fce17e977789d8971a582c7d3c0cc8f890
Ianhetkaasje14 <Ianhetkaasje@ziggo.nl>
2025-09-24 11:23:49 +0200

    Levelgenerator werkt!

--- Fietsgame/Assets/_Scripts/LevelGenerator.cs
+++ Fietsgame/Assets/_Scripts/LevelGenerator.cs
@@ -1,16 +1,33 @@
+using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Level : MonoBehaviour
 {
 
-    void Start()
-    {
+    public GameObject[] segment;
 
-    }
-
-
+    [SerializeField] int zpos = 215;
+    [SerializeField] bool creatingSegments = false;
+    [SerializeField] int segmentNum;
     void Update()
     {
+        if (creatingSegments == false)
+        {
+            creatingSegments = true;
+            StartCoroutine(SegmentGen());
+        }
+    }
+
+    IEnumerator SegmentGen()
+    {
+        segmentNum = Random.Range(0, 4);
+        Instantiate(segment[segmentNum], new Vector3(0, 0, zpos), Quaternion.identity);
+        zpos += 83;
+        yield return new WaitForSeconds(4);
+        creatingSegments = false;
 
     }
 }
